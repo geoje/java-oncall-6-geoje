04249e022708d7fa9c9b22bfc8bac6d607903a33
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 16:22:24 +0900

    feat(domain): 공휴이이 출력되는 테스트 추가 아직 검증은 미구현

--- src/test/java/oncall/ApplicationTest.java
+++ src/test/java/oncall/ApplicationTest.java
@@ -1,10 +1,10 @@
 package oncall;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
 
@@ -68,6 +68,20 @@ class ApplicationTest extends NsTest {
                     "4월 28일 금 조이" + LINE_SEPARATOR,
                     "4월 29일 토 해시" + LINE_SEPARATOR,
                     "4월 30일 일 폴로"
+            );
+        });
+    }
+
+    @Test
+    void 공휴일_테스트() {
+        assertSimpleTest(() -> {
+            run(
+                    "10,토",
+                    "허브,쥬니,말랑,라온,헤나,우코,에단,수달,파워,히이로,마코,슬링키,모디,연어,깃짱,리오,고니,박스터,달리,조이,노아이즈,도이,도치,홍고,스캇,폴로,해시,로지,첵스,아이크,우가,푸만능,애쉬,로이스,오션",
+                    "오션,로이스,애쉬,푸만능,우가,아이크,첵스,로지,해시,폴로,스캇,홍고,도치,도이,노아이즈,조이,달리,박스터,고니,리오,깃짱,연어,모디,슬링키,마코,히이로,파워,수달,에단,우코,헤나,라온,말랑,쥬니,허브"
+            );
+            assertThat(output()).contains(
+                    ""
             );
         });
     }
