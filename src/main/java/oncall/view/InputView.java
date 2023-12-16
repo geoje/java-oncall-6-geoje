3d46de5d87ea61908850bd62370bd04239d35dd7
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 14:05:34 +0900

    feat(const): 기본 입출력에 사용할 View Class 생성 후 질문 메서드 3가지 구현

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,25 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import oncall.constant.GeneralMessage;
+
+public class InputView {
+    private static String readWithGeneralMessage(GeneralMessage message, Object... args) {
+        System.out.printf(message.toString() + "%n", args);
+        return Console.readLine().trim();
+    }
+
+    public static String readMonthDayOfWeek() {
+        return readWithGeneralMessage(GeneralMessage.REQUEST_MONTH_DAY_OF_WEEK);
+    }
+
+    public static String readWeekdayWorkers() {
+        return readWithGeneralMessage(GeneralMessage.REQUEST_WEEKDAY_WORKER);
+    }
+
+    public static String readDayOffWorkers() {
+        String result = readWithGeneralMessage(GeneralMessage.REQUEST_DAY_OFF_WORKER);
+        System.out.printf("%n%n");
+        return result;
+    }
+}
---
+++ src/main/java/oncall/view/OutputView.java
@@ -0,0 +1,20 @@
+package oncall.view;
+
+import oncall.constant.GeneralMessage;
+
+import static oncall.constant.ErrorMessage.EXCEPTION_PREFIX;
+
+public class OutputView {
+
+    private static void printGeneralMessage(GeneralMessage message, Object... args) {
+        System.out.printf(message + "%n", args);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(EXCEPTION_PREFIX + message);
+    }
+
+    public static void printResult() {
+//        printGeneralMessage("");
+    }
+}
