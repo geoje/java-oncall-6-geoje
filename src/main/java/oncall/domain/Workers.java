818c37c063f7ac1a436d77a6646671e80ea6a7cf
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 15:11:15 +0900

    feat(domain): View 에서 부터 넘겨받은 입력 값을 분리자로 분리 후 객체 생성 및 반환 그리고 검증까지 구현

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -2,11 +2,32 @@ package oncall.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static oncall.constant.ErrorMessage.INVALID_VALUE;
 
 public record Workers(List<Worker> workers) {
     private static final String DELIMITER = ",";
+    private static final int MAX_LENGTH = 35;
+
+    public Workers {
+        validateDuplicate(workers);
+        validateLength(workers);
+    }
 
     public static Workers from(String workers) {
         return new Workers(Arrays.stream(workers.split(DELIMITER)).map(Worker::new).toList());
+    }
+
+    private void validateDuplicate(List<Worker> workers) {
+        if (workers.size() != workers.stream().map(Worker::name).collect(Collectors.toSet()).size()) {
+            throw new IllegalArgumentException(INVALID_VALUE.toString());
+        }
+    }
+
+    private void validateLength(List<Worker> workers) {
+        if (workers.size() > MAX_LENGTH) {
+            throw new IllegalArgumentException(INVALID_VALUE.toString());
+        }
     }
 }
