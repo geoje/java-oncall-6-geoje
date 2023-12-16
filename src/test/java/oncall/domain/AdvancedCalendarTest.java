7d4bd3368de43db99f15ccb80e480c0e14454105
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 15:09:20 +0900

    test(domain): 내가 만든 달력 객체 테스트에서 DisplayName 에 예외라는 단어를 넣어 가독성 향상

--- src/test/java/oncall/domain/AdvancedCalendarTest.java
+++ src/test/java/oncall/domain/AdvancedCalendarTest.java
@@ -27,7 +27,7 @@ public class AdvancedCalendarTest {
     }
 
     @ParameterizedTest
-    @DisplayName("입력 인자가 충분치 않을 경우")
+    @DisplayName("입력 인자가 충분치 않을 경우 예외")
     @ValueSource(strings = {"12 월"})
     void validateEnough(String monthDayOfWeek) {
         assertThatCode(() -> AdvancedCalendar.from(monthDayOfWeek))
@@ -35,7 +35,7 @@ public class AdvancedCalendarTest {
     }
 
     @ParameterizedTest
-    @DisplayName("월에 숫자가 아닌 것이 입력 되었을 경우")
+    @DisplayName("월에 숫자가 아닌 것이 입력 되었을 경우 예외")
     @ValueSource(strings = {"십이,일"})
     void validateMonthNumeric(String monthDayOfWeek) {
         assertThatCode(() -> AdvancedCalendar.from(monthDayOfWeek))
@@ -43,7 +43,7 @@ public class AdvancedCalendarTest {
     }
 
     @ParameterizedTest
-    @DisplayName("월이 달력에 존재하지 않는 숫자일 경우")
+    @DisplayName("월이 달력에 존재하지 않는 숫자일 경우 예외")
     @ValueSource(strings = {"-1,일", "13,일"})
     void validateMonthInCalendar(String monthDayOfWeek) {
         assertThatCode(() -> AdvancedCalendar.from(monthDayOfWeek))
@@ -51,7 +51,7 @@ public class AdvancedCalendarTest {
     }
 
     @ParameterizedTest
-    @DisplayName("요일이 달력에 존재하지 않을 경우")
+    @DisplayName("요일이 달력에 존재하지 않을 경우 예외")
     @ValueSource(strings = {"12, 왈", "12, 슈"})
     void validateDayOfWeekInCalendar(String monthDayOfWeek) {
         assertThatCode(() -> AdvancedCalendar.from(monthDayOfWeek))
