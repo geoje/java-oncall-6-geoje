3fce69be35d7525c33787fd984d6619fb9a66dca
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 14:38:39 +0900

    feat(domain): 기존 Calendar 를 사용하여 나만의 요일로 정하여 사용할 수 있는 AdvancedCalendar Record 생성

---
+++ src/main/java/oncall/domain/AdvancedCalendar.java
@@ -0,0 +1,50 @@
+package oncall.domain;
+
+import oncall.constant.DayOfWeekKo;
+
+import java.util.Calendar;
+import java.util.regex.Pattern;
+
+import static oncall.constant.ErrorMessage.INVALID_VALUE;
+
+public record AdvancedCalendar(int month, DayOfWeekKo dayOfWeek) {
+    private static final String DELIMITER = ",";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
+    private static final Calendar CAL = Calendar.getInstance();
+    private static final int YEAR = 2023;
+    private static final int MIN_MONTH = 1;
+    private static final int MAX_MONTH = 12;
+
+    public AdvancedCalendar {
+        CAL.set(Calendar.YEAR, YEAR);
+    }
+
+    public static AdvancedCalendar from(String monthDayOfWeek) {
+        validateCount(monthDayOfWeek);
+        String month = monthDayOfWeek.split(DELIMITER)[0];
+        String dayOfWeek = monthDayOfWeek.split(DELIMITER)[1];
+        validateNumeric(month);
+        validateMonth(month);
+
+        return new AdvancedCalendar(Integer.parseInt(month), DayOfWeekKo.from(dayOfWeek));
+    }
+
+    private static void validateNumeric(String month) {
+        if (!NUMBER_PATTERN.matcher(month).matches()) {
+            throw new IllegalArgumentException(INVALID_VALUE.toString());
+        }
+    }
+
+    private static void validateMonth(String month) {
+        int parsedMonth = Integer.parseInt(month);
+        if (parsedMonth < MIN_MONTH || parsedMonth > MAX_MONTH) {
+            throw new IllegalArgumentException(INVALID_VALUE.toString());
+        }
+    }
+
+    private static void validateCount(String monthDayOfWeek) {
+        if (monthDayOfWeek.split(DELIMITER).length < 2) {
+            throw new IllegalArgumentException(INVALID_VALUE.toString());
+        }
+    }
+}
