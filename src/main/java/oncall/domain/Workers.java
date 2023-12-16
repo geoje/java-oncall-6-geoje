81f9ab6ccce6060bab7d603191c62de43fd5d7f5
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 17:16:26 +0900

    feat(domain): Workers Record를 calss로 변경한 후 가변적으로 pick 할 수 있도록 index 필드 추가

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -6,18 +6,53 @@ import java.util.stream.Collectors;
 
 import static oncall.constant.ErrorMessage.INVALID_VALUE;
 
-public record Workers(List<Worker> workers) {
+public class Workers {
     private static final String DELIMITER = ",";
     private static final int MIN_LENGTH = 5;
     private static final int MAX_LENGTH = 35;
 
-    public Workers {
+    private final List<Worker> workers;
+    private int index = 0;
+    private boolean change = false;
+
+    public Workers(List<Worker> workers) {
+        this.workers = workers;
         validateDuplicate(workers);
         validateLength(workers);
     }
 
     public static Workers from(String workers) {
         return new Workers(Arrays.stream(workers.split(DELIMITER)).map(Worker::new).toList());
+    }
+
+    private int nextIndex() {
+        return (index + 1) % workers.size();
+    }
+
+    private int prevIndex() {
+        if (index == 0) return workers.size() - 1;
+        return index - 1;
+    }
+
+    private Worker nextWithChange() {
+        if (change) {
+            return workers.get(prevIndex());
+        }
+        return workers.get(index);
+    }
+
+    public Worker next(String avoidWorkerName) {
+        Worker worker = nextWithChange();
+        change = false;
+
+        if (worker.name().equals(avoidWorkerName)) {
+            change = true;
+            index = nextIndex();
+            return workers.get(index);
+        }
+
+        index = nextIndex();
+        return worker;
     }
 
     private void validateDuplicate(List<Worker> workers) {
