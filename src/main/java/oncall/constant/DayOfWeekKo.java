d8416f3491ad208180f59edc17320d67f3bf2abb
Gyeongho Yang <chchch1213@naver.com>
2023-12-16 16:21:05 +0900

    fix(const): DayOfWeek 을 잘못 참조하고 있던 오류 Calendar 를 참조하도록 수정

--- src/main/java/oncall/constant/DayOfWeekKo.java
+++ src/main/java/oncall/constant/DayOfWeekKo.java
@@ -1,23 +1,23 @@
 package oncall.constant;
 
-import java.time.DayOfWeek;
 import java.util.Arrays;
+import java.util.Calendar;
 
 import static oncall.constant.ErrorMessage.INVALID_VALUE;
 
 public enum DayOfWeekKo {
-    SUNDAY(DayOfWeek.SUNDAY, "일"),
-    MONDAY(DayOfWeek.MONDAY, "월"),
-    TUESDAY(DayOfWeek.TUESDAY, "화"),
-    WEDNESDAY(DayOfWeek.WEDNESDAY, "수"),
-    THURSDAY(DayOfWeek.THURSDAY, "목"),
-    FRIDAY(DayOfWeek.FRIDAY, "금"),
-    SATURDAY(DayOfWeek.SATURDAY, "토");
+    SUNDAY(Calendar.SUNDAY, "일"),
+    MONDAY(Calendar.MONDAY, "월"),
+    TUESDAY(Calendar.TUESDAY, "화"),
+    WEDNESDAY(Calendar.WEDNESDAY, "수"),
+    THURSDAY(Calendar.THURSDAY, "목"),
+    FRIDAY(Calendar.FRIDAY, "금"),
+    SATURDAY(Calendar.SATURDAY, "토");
 
-    private final DayOfWeek dayOfWeek;
+    private final int dayOfWeek;
     private final String name;
 
-    DayOfWeekKo(DayOfWeek dayOfWeek, String name) {
+    DayOfWeekKo(int dayOfWeek, String name) {
         this.dayOfWeek = dayOfWeek;
         this.name = name;
     }
@@ -29,7 +29,24 @@ public enum DayOfWeekKo {
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_VALUE.toString()));
     }
 
-    public DayOfWeek getDayOfWeek() {
+    public static DayOfWeekKo from(int dayOfWeek) {
+        int min = Arrays.stream(values()).mapToInt(value -> value.dayOfWeek).min().getAsInt();
+        int max = Arrays.stream(values()).mapToInt(value -> value.dayOfWeek).max().getAsInt();
+
+        while (dayOfWeek < min) {
+            dayOfWeek += values().length;
+        }
+        while (dayOfWeek > max) {
+            dayOfWeek -= values().length;
+        }
+
+        int finalDayOfWeek = dayOfWeek;
+        return Arrays.stream(values()).filter(v -> v.dayOfWeek == finalDayOfWeek)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_VALUE.toString()));
+    }
+
+    public int getValue() {
         return dayOfWeek;
     }
 
